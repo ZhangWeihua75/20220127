575578d8d21ef74d06f892d43c9501133e39d729
zwh <weihua.zhang75@gmail.com>
2022-01-30 17:29:48 -0500

    commit -m "zwh"

--- Python-1.py
+++ Python-1.py
@@ -17,3 +17,31 @@ a2="4"
 b2="6"
 print("a1+b1=",a1+b1)
 print("But a2+b2=",a2+b2)
+#字符串及转义字符练习
+a3='I\'m a student.'
+a4="I'm a student."
+a5="我正在学习\nCSDN的PYTHON课程！"
+print("使用转义符\'的字符串输出方法",a3)
+print("未使用转义符\'的字符串输出方法",a4)
+print(a5)
+a6="我正在学习\
+CSDN的PYTHON课程！"
+print(a6) 
+#字符串长度和切片练习
+s="Python很有趣，而且也不难！"   
+print(len(s))
+s1=s[:5]
+s2=s[0]
+print(s1)
+print(s2)
+s3=s[3:8:2]
+s4=s[3:8]
+print(s3)
+print(s4)
+s5=s[5:]
+print(s5)
+ss="PYTHON非常有趣"
+s6=ss[0:6]
+s7=ss[6:]
+print(s6)
+print(s7)

d3a7f05664f0f26101fa2ea8391e4498f0731763
zwh <weihua.zhang75@gmail.com>
2022-02-01 21:44:46 -0500

    commit

--- 例题3.py
+++ 例题3.py
@@ -7,8 +7,14 @@ print("我买了%s斤%s" %(n,s))
 print("我买了{}斤{}".format(n,s))
 
 #排错
-
 aa=input("请输入一个数值")
 print("Result=",int(aa)+2)
-
 print(aa*2)
+
+#课后作业 分别输入a5=4和b5=6,让其结果是46 和10
+a5=input("请输入数值1:")
+b5=input("请输入数值2:")
+print("a5+b5=",a5+b5)
+print("a5+b5=",int(a5)+int(b5))
+
+

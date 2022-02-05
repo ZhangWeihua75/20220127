0dff3a502eeec90084ab495c30a85d9f5cd1c2ec
zwh <weihua.zhang75@gmail.com>
2022-02-05 16:46:51 -0500

    commit

--- break-1.py
+++ break-1.py
@@ -21,3 +21,17 @@ if flag==1:
     print("\nIn the list,there is 0!")       
 else:
     print("\nIn the list,there isn't 0!")       
+#利用break，判断列表内是否存在奇数
+z=[2,10,14,100,17,18,35,102]    
+flag=0
+num=1
+for b in z:
+    print("循环进行了",num,"次")
+    if b%2==1:
+       flag=1
+       break
+    num+=1
+if flag==1:   
+    print("In List,there is odd number!")
+else:   
+    print("In List,there isn't odd number!")    

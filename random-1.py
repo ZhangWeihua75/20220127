ef862a57da1fb9989b6dc8f7b82bfdc514337027
zwh <weihua.zhang75@gmail.com>
2022-02-08 20:35:52 -0500

    commit

--- random-1.py
+++ random-1.py
@@ -2,7 +2,7 @@ import random
 a=random.randrange(1,20,3)
 print(a)
 
-print(random.randint(1,10))  #incloude 10
+print(random.randint(1,10))  #include 10
 
 # random.choice() 
 x=["CSDN","Olympia","Hello","Bye-Bye"]
@@ -11,7 +11,7 @@ print(random.choice(x))
 y=range(50)
 z=list(y)
 print("z:",z)
-random.shuffle(z)
+random.shuffle(z)  #shuffle()的返回值是个NONE,只是将列表中的数据进行操作
 print("z:",z)
 
 #random.sample() 取样
@@ -24,5 +24,9 @@ print(random.randrange(100,200,2))
 #讲1-100之间的数随机排列
 c=range(100)
 c1=list(c)
-random.shuffle(c1)
+random.shuffle(c1) #操作的对象是LIST（C1的值）
 print(c1)
+
+#uniform(a,b))产生一个ab之间的随机数
+print(random.uniform(9,10))
+print(random.random())#产生0---1之间的一个随机数

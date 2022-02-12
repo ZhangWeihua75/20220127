b27d487b5ca14164008c1d536bde2e33f10ceff6
zwh <weihua.zhang75@gmail.com>
2022-02-12 16:06:14 -0500

    commit

--- defModule.py
+++ defModule.py
@@ -1,2 +1,15 @@
+import math
 def test():
      print("Hello world!")
+     
+def s(n):
+    print(n*n)
+    
+def circle(r):
+    print(math.pi*r*r)
+def square(l):
+    print(l*l)    
+def rectangle(a,b):
+    print(a*b)     
+def trapezoid(a,b,h):
+    return (a+b)*h/2    
--- useModule.py
+++ useModule.py
@@ -1,2 +1,9 @@
 import defModule
 defModule.test()
+defModule.s(10)
+
+defModule.circle(5)
+defModule.square(5)
+defModule.rectangle(5,8)
+
+print("The surface of is:",defModule.trapezoid(5,8,4))

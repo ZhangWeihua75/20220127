c2c47f08500fefc4536fb975948554fc6e0fa956
zwh <weihua.zhang75@gmail.com>
2022-02-26 22:12:40 -0500

    commit

--- file3.py
+++ file3.py
@@ -1,5 +1,12 @@
 #存储文件
 file="file4.txt"
 with open(file,mode="w") as f:
-    f.write("张三888888")
+    f.write("888888")
     print(f.read())
+    
+x=open("file2.txt","r")
+y=open("zwh.txt","w" )
+
+y.writelines(x.readlines())
+x.close()
+y.close()
